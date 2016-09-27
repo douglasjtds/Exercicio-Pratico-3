062865405744f4466c076d41f01abaef6f0923c3
Douglas Tertuliano <douglasjtds@gmail.com>
2016-09-27 00:46:34 -0300

    commitando projeto do EP 3.1

---
+++ EP 3.1 (Com ArrayList-HashMap)/src/EP3/Reporter.java
@@ -0,0 +1,37 @@
+package EP3;
+
+/**
+ *
+ * @author douglasjtds
+ */
+public class Reporter {
+    
+    private String nomeCompletoReporter;
+    private int idSequencial;
+    
+    public int getIdSequencial() {
+        return idSequencial;
+    }
+
+    public void setIdSequencial(int idSequencial) {
+        this.idSequencial = idSequencial;
+    }
+
+    public String getNomeCompletoReporter() {
+        return nomeCompletoReporter;
+    }
+
+    public void setNomeCompletoReporter(String nomeCompletoReporter) {
+        this.nomeCompletoReporter = nomeCompletoReporter;
+    }
+ 
+    public Reporter(String nomeCompletoReporter, int idSequencial) {
+        this.nomeCompletoReporter = nomeCompletoReporter;
+        this.idSequencial = idSequencial;
+    }
+    
+    public String imprimeReporteres() {
+        return "Nome do repórter: " + nomeCompletoReporter + "\n" +
+               "ID: " + idSequencial + "\n";
+    }
+}
